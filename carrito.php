8ed807ab0e7451a8cd511819737733b7a7aabe6f
Ricardo Jarro <ricardo.jarro98@ucuenca.edu.ec>
2020-06-02 23:03:43 -0500

    estructura carrito finalizada

---
+++ carrito.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Carrito compras</title>
+    <link rel="stylesheet" href="css/estilo_carrito.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/63fafe2dae.js" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+
+    <div class="container-fluid">
+        <div class="row">
+            <!-- PRODUCTOS EN LA CESTA-PARTE IZQUIERDA -->
+            <div class="col col-12 col-sm-12 col-md-7 col-lg-7 container-iquierdo">
+                <div class="position-relative">
+                    <h2 class="display-4">Cesta de productos</h2>
+                    <p id="btn-vaciar-carrito"><i class="fas fa-trash mano"></i></p>
+                    <hr class="separador-carrito">
+                </div>
+
+                <div class="container carrito w-100">
+                    <!-- Plantila-->
+                    <div class="row item-carrito border rounded">
+                        <div class="container-imagen d-inline-block">
+                            <img src="images/camisa-rubik_1_negro.jpg" class="h-100">
+                        </div>
+                        <div class="info-item-carrito d-inline-block ">
+                            <h5><a href="#">Camiseta Rubik</a></h6>
+                                <h6 class="d-inline">Color: </h6>
+                                <h7 class="d-inline">negro</h6>
+                                    <br>
+                                    <h6 class="d-inline-block">Talla: </h6>
+                                    <select name="" id="tallas-disponibles">
+                                        <option value="">XS</option>
+                                        <option value="">S</option>
+                                        <option value="">M</option>
+                                        <option value="">X</option>
+                                    </select>
+                                    <br>
+                                    <h5 class="d-inline-block">19.99 $</h5>
+                        </div>
+                        <div class="modificar-cantidad d-inline-block ">
+                            <button type="button" class="btn-circular d-inline-block border-0"><i class="fas fa-minus"></i></button>
+                            <p class=" cantidad-producto d-inline-block">1</p>
+                            <button type="button" class="btn-circular d-inline-block  border-0"><i class="fas fa-plus"></i></button>
+                        </div>
+                        <button type="button" class="btn-danger border-0 btn-elimina-producto"><i class="fas fa-trash"></i></button>
+                    </div>
+                    <!-- Plantila-->
+
+                    <!-- Copias -->
+
+                    <div class="row item-carrito border rounded">
+                        <div class="container-imagen d-inline-block">
+                            <img src="images/camisa-rubik_1_negro.jpg" class="h-100">
+                        </div>
+                        <div class="info-item-carrito d-inline-block ">
+                            <h5><a href="#">Camiseta Rubik</a></h6>
+                                <h6 class="d-inline">Color: </h6>
+                                <h7 class="d-inline">negro</h6>
+                                    <br>
+                                    <h6 class="d-inline-block">Talla: </h6>
+                                    <select name="" id="tallas-disponibles">
+                                        <option value="">XS</option>
+                                        <option value="">S</option>
+                                        <option value="">M</option>
+                                        <option value="">X</option>
+                                    </select>
+                                    <br>
+                                    <h5 class="d-inline-block">19.99 $</h5>
+                        </div>
+                        <div class="modificar-cantidad d-inline-block ">
+                            <button type="button" class="btn-circular d-inline-block border-0"><i class="fas fa-minus"></i></button>
+                            <p class=" cantidad-producto d-inline-block">1</p>
+                            <button type="button" class="btn-circular d-inline-block  border-0"><i class="fas fa-plus"></i></button>
+                        </div>
+                        <button type="button" class="btn-danger border-0 btn-elimina-producto"><i class="fas fa-trash"></i></button>
+                    </div>
+
+
+                    <div class="row item-carrito border rounded">
+                        <div class="container-imagen d-inline-block">
+                            <img src="images/camisa-rubik_1_negro.jpg" class="h-100">
+                        </div>
+                        <div class="info-item-carrito d-inline-block ">
+                            <h5><a href="#">Camiseta Rubik</a></h6>
+                                <h6 class="d-inline">Color: </h6>
+                                <h7 class="d-inline">negro</h6>
+                                    <br>
+                                    <h6 class="d-inline-block">Talla: </h6>
+                                    <select name="" id="tallas-disponibles">
+                                        <option value="">XS</option>
+                                        <option value="">S</option>
+                                        <option value="">M</option>
+                                        <option value="">X</option>
+                                    </select>
+                                    <br>
+                                    <h5 class="d-inline-block">19.99 $</h5>
+                        </div>
+                        <div class="modificar-cantidad d-inline-block ">
+                            <button type="button" class="btn-circular d-inline-block border-0"><i class="fas fa-minus"></i></button>
+                            <p class=" cantidad-producto d-inline-block">1</p>
+                            <button type="button" class="btn-circular d-inline-block  border-0"><i class="fas fa-plus"></i></button>
+                        </div>
+                        <button type="button" class="btn-danger border-0 btn-elimina-producto"><i class="fas fa-trash"></i></button>
+                    </div>
+
+                    <div class="row item-carrito border rounded">
+                        <div class="container-imagen d-inline-block">
+                            <img src="images/camisa-rubik_1_negro.jpg" class="h-100">
+                        </div>
+                        <div class="info-item-carrito d-inline-block ">
+                            <h5><a href="#">Camiseta Rubik</a></h6>
+                                <h6 class="d-inline">Color: </h6>
+                                <h7 class="d-inline">negro</h6>
+                                    <br>
+                                    <h6 class="d-inline-block">Talla: </h6>
+                                    <select name="" id="tallas-disponibles">
+                                        <option value="">XS</option>
+                                        <option value="">S</option>
+                                        <option value="">M</option>
+                                        <option value="">X</option>
+                                    </select>
+                                    <br>
+                                    <h5 class="d-inline-block">19.99 $</h5>
+                        </div>
+                        <div class="modificar-cantidad d-inline-block ">
+                            <button type="button" class="btn-circular d-inline-block border-0"><i class="fas fa-minus"></i></button>
+                            <p class=" cantidad-producto d-inline-block">1</p>
+                            <button type="button" class="btn-circular d-inline-block  border-0"><i class="fas fa-plus"></i></button>
+                        </div>
+                        <button type="button" class="btn-danger border-0 btn-elimina-producto"><i class="fas fa-trash"></i></button>
+                    </div>
+                    <!-- Copias -->
+
+                </div>
+
+            </div>
+
+            <!-- EESUMEN DEL PEDIDO-->
+            <div class="col col-12 col-sm-12 col-md-5 col-lg-5 border-0">
+                <div id="container-resumen-carrito" class=" border rounded ">
+                    <h4>Resumen del pedido</h4>
+                    <div class="mt-3 position-relative">
+                        <p class="texto-resumen-pedido">Subtotal</p>
+                        <p class="valor-resumen" id="subtotal-resumen-pedido"><b>49,99</b></p>
+                    </div>
+                    <div class="position-relative">
+                        <p class="texto-resumen-pedido">Envio</p>
+                        <p class="valor-resumen" id="envio-resumen-pedido"><b>4,99</b></p>
+                    </div>
+                    <hr>
+                    <div class="position-relative">
+                        <p class="texto-resumen-pedido"><b>Total</b></p>
+                        <p class="valor-resumen" id="total-resumen-pedido"><b>54,98</b></p>
+                    </div>
+                    <div class="mt-3 centrar-forzado"><button type="button" class="btn btn-danger">Comprar</button></div>
+                    
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!-- LIBRERIAS -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-zoom/1.7.21/jquery.zoom.js"></script>
+    <!-- FIN-->
+
+    <!-- CODIGO JS -->
+
+    <!-- FIN -->
+</body>
+
+</html>
---
+++ producto.php
@@ -0,0 +1,240 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="css/estilo_producto.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/63fafe2dae.js" crossorigin="anonymous"></script>
+
+
+
+</head>
+
+<body>
+    <div class="container">
+
+        <div class="row">
+            <div class="col col-12 col-sm-12 col-md-7 col-lg-6 container-imagenes-min">
+                <div class="producto-imagen-grande zoom">
+                    <img src="images/camisa-tiburon_1_blanco.jpg" alt="" class="zoom img-responsive" id="img-grande">
+                </div>
+
+                <div id="lista-imagenes">
+                    <img src="images/camisa-tiburon_1_blanco.jpg" id="img-1" class="miniatura activado mano">
+                    <img src="images/camisa-tiburon_2_blanco.jpg" id="img-2" class="miniatura mano">
+                    <img src="images/camisa-tiburon_3_blanco.jpg" id="img-3" class="miniatura mano">
+                </div>
+
+            </div>
+            <div class="col col-12 col-sm-12 col-md-5 col-lg-6 container-info">
+                <div id="deter-producto-nombre">
+                    <h2>Camiseta Baby Shark</h2>
+                </div>
+                <div id="deter-producto-descripcion">
+                    <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non veritatis corporis quaerat. Perspiciatis nihil fugiat sit nostrum odio eaque tenetur nemo cupiditate quidem eligendi ex vitae distinctio velit, quasi illum.</h4>
+                </div>
+                <div id="deter-producto-precio">
+                    <h3>19.99 $</h3>
+                </div>
+                <h4>Color</h4>
+                <div id="determ-producto-colores">
+                    <button class="boton-color amarillo"></button>
+                    <button class="boton-color azul"></button>
+                    <button class="boton-color blanco"></button>
+                    <button class="boton-color negro"></button>
+                </div>
+                <div class="container-talla">
+                    <h4>Talla</h4>
+                    <div class="opc-talla">
+                        <select name="" id="tallas-disponibles">
+
+                        </select>
+                    </div>
+                </div>
+                <div class="container-cantidad">
+
+                    <h4>Cantidad</h4>
+
+                    <div class="texto-no-seleccionable mano" id="btn-menos"><i class="fas fa-minus"></i></div>
+                    <div class="texto-no-seleccionable" id="id_num_prenda">1</div>
+                    <div class="texto-no-seleccionable mano" id="btn-mas"><i class="fas fa-plus"></i></div>
+                </div>
+
+                <button type="button" class="btn btn-primary añadir-carrito d-block"><i class="fas fa-shopping-cart"></i>Añadir al carrito</button>
+
+            </div>
+
+        
+        </div>
+
+        <div class="similar-product">
+             <h5>Productos similares</h5>
+        
+        <div class="slider-items">
+           
+
+            <div class="item-recommend bg-info d-inline-block mano">
+                <img src="images/camisa-rubik_1_negro.jpg" alt="" class="img-product-recommend">
+                <div class="article__name" title="Camiseta hombre">
+                    <h5>Camiseta Rubik hombre</h5>
+                </div>
+                <div class="price__now-price">
+                    <h5>19.99$</h5>
+                </div>
+            </div>
+
+            <div class="item-recommend bg-info d-inline-block mano">
+                <img src="images/camisa-rubik_1_negro.jpg" alt="" class="img-product-recommend">
+                <div class="article__name" title="Camiseta hombre">
+                    <h5>Camiseta Rubik hombre</h5>
+                </div>
+                <div class="price__now-price">
+                    <h5>19.99$</h5>
+                </div>
+            </div>
+            <div class="item-recommend bg-info d-inline-block mano">
+                <img src="images/camisa-rubik_1_negro.jpg" alt="" class="img-product-recommend">
+                <div class="article__name" title="Camiseta hombre">
+                    <h5>Camiseta Rubik hombre</h5>
+                </div>
+                <div class="price__now-price">
+                    <h5>19.99$</h5>
+                </div>
+            </div>
+            <div class="item-recommend bg-info d-inline-block mano">
+                <img src="images/camisa-rubik_1_negro.jpg" alt="" class="img-product-recommend">
+                <div class="article__name" title="Camiseta hombre">
+                    <h5>Camiseta Rubik hombre</h5>
+                </div>
+                <div class="price__now-price">
+                    <h5>19.99$</h5>
+                </div>
+            </div>
+
+            <div class="item-recommend bg-info d-inline-block mano">
+                <img src="images/camisa-rubik_1_negro.jpg" alt="" class="img-product-recommend">
+                <div class="article__name" title="Camiseta hombre">
+                    <h5>Camiseta Rubik hombre</h5>
+                </div>
+                <div class="price__now-price">
+                    <h5>19.99$</h5>
+                </div>
+            </div>
+
+            <div class="item-recommend bg-info d-inline-block mano">
+                <img src="images/camisa-rubik_1_negro.jpg" alt="" class="img-product-recommend">
+                <div class="article__name" title="Camiseta hombre">
+                    <h5>Camiseta Rubik hombre</h5>
+                </div>
+                <div class="price__now-price">
+                    <h5>19.99$</h5>
+                </div>
+            </div>
+        </div>
+
+        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque autem repellat sequi illo! At quas minima est modi itaque sint id ullam, recusandae a consequatur facilis quod illum, maxime ad!</div>
+
+
+    </div>
+
+
+
+    </div>
+    </div>
+    </div>
+    </div>
+
+<!-- LIBRERIAS -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-zoom/1.7.21/jquery.zoom.js"></script>
+
+<!-- FIN-->
+
+
+<!-- Para el efecto zoom sobre la imagen -->
+<script>
+    // Este script intercambia las imagenes y hace el efecto zoom
+    $(document).ready(
+
+
+        function() {
+            var img_actual = document.getElementById("img-grande").src;
+            $('.producto-imagen-grande').zoom({
+                url: img_actual,
+                magnify: 1.5
+            });
+            $(function() {
+                $('.miniatura').click(function() {
+                    var id = $(this).attr('id');
+                    $('#lista-imagenes').children().removeClass("activado");
+                    $("#" + id).addClass("activado");
+                    img_actual = document.getElementById(id).src;
+                    $('#img-grande').attr('src', img_actual);
+                    $('.producto-imagen-grande').zoom({
+                        url: img_actual,
+                        magnify: 1.5
+                    });
+                });
+            });
+
+            $(function() {
+                $('.boton-color').click(function() {
+                    var colorR = $(this).attr('class').split(/\s+/);
+                    colorReq = colorR[1];
+                    var ubicacion = $('#img-grande').attr('src').split("_");
+                    $('#img-grande').attr('src', ubicacion[0] + "_" + ubicacion[1] + "_" + colorR[1] + ".jpg");
+                    $('#img-1').attr('src', ubicacion[0] + "_1_" + colorR[1] + ".jpg");
+                    $('#img-2').attr('src', ubicacion[0] + "_2_" + colorR[1] + ".jpg");
+                    $('#img-3').attr('src', ubicacion[0] + "_3_" + colorR[1] + ".jpg");
+
+                    $('.producto-imagen-grande').zoom({
+                        url: ubicacion[0] + "_" + ubicacion[1] + "_" + colorR[1] + ".jpg",
+                        magnify: 1.5
+                    });
+
+                });
+            });
+
+            $(function() {
+                var tallas = ["XS", "S", "M", "L", "XL", "XXL"];
+                for (var i = 0; i < tallas.length; i++) {
+                    var option = document.createElement("option");
+                    $(option).html(tallas[i]);
+                    $(option).appendTo("#tallas-disponibles");
+                }
+            });
+
+            $(function() {
+                $('#btn-menos').click(function() {
+                    var n = parseInt($('#id_num_prenda').text()) - 1;
+                    if (n <= 0) {
+                        n = 1;
+                    }
+                    $('#id_num_prenda').text(n.toString());
+                });
+
+                $('#btn-mas').click(function() {
+                    var n = parseInt($('#id_num_prenda').text()) + 1;
+                    if (n >= 50) {
+                        n = n - 1;
+                    }
+                    $('#id_num_prenda').text(n.toString());
+                });
+            });
+
+        });
+</script>
+
+
+
+</body>
+
+
+
+</html>
